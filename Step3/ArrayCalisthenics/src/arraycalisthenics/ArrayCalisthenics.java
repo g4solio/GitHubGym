c509ef7fb94b4fc368b668f21fad40452af1a8bb
Marcello Missiroli <marcello.missiroli@gmail.com>
2017-02-10 16:30:47 +0100

    Aggiunto scheletro del menu

--- Step3/ArrayCalisthenics/src/arraycalisthenics/ArrayCalisthenics.java
+++ Step3/ArrayCalisthenics/src/arraycalisthenics/ArrayCalisthenics.java
@@ -1,9 +1,34 @@
 package arraycalisthenics;
 
+import java.util.Scanner;
+
 public class ArrayCalisthenics {
     final int SIZE=10;
-    
+    /**
+     * 
+     * @param args 
+     * Questo è solo uno schelettro del programma.
+     * Andrà modificato
+     */
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        do {
+        System.out.println("\n\n          Menu");
+        System.out.println("--------------------------------------");
+        System.out.println("1 - AZIONE 1");
+        System.out.print("\nScegli una voce dal menu: ");
+    try {
+        int i = Integer.parseInt(input.next());
+
+
+        switch (i) {
+        case 1:  //TODO
+                 break;
+       }
+      } catch (NumberFormatException e) {  }
+
+    }
+    while(true); 
         
     }
     
